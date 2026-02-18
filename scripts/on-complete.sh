cb6e2547c88fd44ade33e641eeae3dbf41de5e61
Huw <HuwSy@users.noreply.github.com>
2026-02-18 19:40:29 +0000

    Update on-complete.sh

--- scripts/on-complete.sh
+++ scripts/on-complete.sh
@@ -8,17 +8,17 @@ FIRST=$(printf "%s" "$LABEL" | cut -c1 | tr '[:lower:]' '[:upper:]')
 REST=$(printf "%s" "$LABEL" | cut -c2-)
 CAP_LABEL="${FIRST}${REST}"
 
-if [ "$CAP_LABEL" = "Film" ] || [ "$CAP_LABEL" = "Movie" ] || [ "$CAP_LABEL" = "Movies" ]; then
+if [ "$CAP_LABEL" = "Films" ] || [ "$CAP_LABEL" = "Film" ] || [ "$CAP_LABEL" = "Movie" ] || [ "$CAP_LABEL" = "Movies" ]; then
   FIRST_CHAR=$(printf "%s" "$TR_TORRENT_NAME" | cut -c1 | tr '[:lower:]' '[:upper:]')
   if ! printf "%s" "$FIRST_CHAR" | grep -q '[A-Z]'; then
     FIRST_CHAR="0-9"
   fi
   CAP_LABEL="Films/$FIRST_CHAR"
-elif [ "$CAP_LABEL" = "Tv" ] || [ "$CAP_LABEL" = "Television" ] || [ "$CAP_LABEL" = "Shows" ]; then
+elif [ "$CAP_LABEL" = "TV" ] || [ "$CAP_LABEL" = "Tv" ] || [ "$CAP_LABEL" = "Television" ] || [ "$CAP_LABEL" = "Shows" ]; then
   SHOW_NAME=$(printf "%s" "$TR_TORRENT_NAME" | sed -E 's/([Ss][0-9]+|[Yy][0-9]{4}|[Ss]eason.*|[Ee]pisode.*|[Pp]art.*|[Vv]olume.*|[Dd]isc.*).*//')
   SHOW_NAME=$(printf "%s" "$SHOW_NAME" | sed -E 's/[^A-Za-z]+/ /g' | sed -E 's/(^| )([a-z])/\U\2/g')
   CAP_LABEL="TV/$SHOW_NAME"
-elif [ "$CAP_LABEL" = "Songs" ] || [ "$CAP_LABEL" = "Albums" ] || [ "$CAP_LABEL" = "Singles" ]; then
+elif [ "$CAP_LABEL" = "Music" ] || [ "$CAP_LABEL" = "Songs" ] || [ "$CAP_LABEL" = "Albums" ] || [ "$CAP_LABEL" = "Singles" ]; then
   MUSIC_NAME=$(printf "%s" "$TR_TORRENT_NAME" | sed -E 's/\.[^.]+$//')
   CAP_LABEL="Music/$MUSIC_NAME"
 fi
