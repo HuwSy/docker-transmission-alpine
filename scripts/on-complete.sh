0a75aad9a05233f6fadac957812c112112481404
Huw <HuwSy@users.noreply.github.com>
2026-05-19 14:56:02 +0100

    Fetch and assign name from Transmission info
    Update the script to fetch and assign the name from Transmission.

--- scripts/on-complete.sh
+++ scripts/on-complete.sh
@@ -41,7 +41,8 @@ printf '%s: %s (%s) Completed ratio %s in %s min\n' "$(timestamp)" "$NAME" "$LAB
 Tinfo=$(transmission-remote "localhost:${WEBUI}" --auth "${WEBUSER}:${WEBPASS}" -t "$TID" 2>/dev/null || true)
 NLABEL=$(printf '%s\n' "$Tinfo" | awk 'BEGIN{found=0} /^[[:space:]]*Labels:/ {found=1; next} found && NF{ gsub(/^ *| *$/,""); split($0,a,","); print a[1]; exit }')
 [ -n "$NLABEL" ] && LABEL="$NLABEL"
-NAME=$(printf '%s\n' "$Tinfo" | awk -F': ' '/^[[:space:]]*Name:/ {print $2; exit}')
+NNAME=$(printf '%s\n' "$Tinfo" | awk -F': ' '/^[[:space:]]*Name:/ {print $2; exit}')
+[ -n "$NNAME" ] && NAME="$NNAME"
 
 [ -z "$LABEL" ] && exit 0
 
